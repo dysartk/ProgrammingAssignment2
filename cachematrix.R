b257b0e99b346d3cd097b3d5758d4811cf24edef
dysartk <dysartk@email.chop.edu>
2015-01-18 11:37:44 -0500

    Test Changes to cachematrix.R
    Simply added a line to the cloned file in the repository to test the
    method of submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## First edit to test the submission method.
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
