a423b1946bb26539dc851169185ada80325d8f47
dysartk <dysartk@email.chop.edu>
2015-01-24 16:33:41 -0500

    Operational Functions
    Modifications of the examples that function in R.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-## First edit to test the submission method.
-## Write a short comment describing this function
+## makeCacheMatrix does a similar job as the original makeCacheMean function.
+## cacheSolve repeats the similarity for solving the inverse of the matrix.
+##
+## makeCacheMatrix does the same job here as the example code did for the makeVector 
+## function. Here a method to hold the value of the inverse of a matrix 
+## we create is defined. The return of this function is really list of 4 functions as
+## before. Here the functions are re-named to reflect the task they complete.
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i = NULL
+	set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) i <<- solve
+        getsolve <- function() i
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function basically takes the matrix assigned when calling the above function and
+## first checks to see if the i contains, NULL, and if so return the value stored. If 
+## not then the inverse of the matrix is calculate by assigning the matrix from the x$get
+## to data and then assigning the inverse to i.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getsolve()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setsolve(i)
+        i
 }
